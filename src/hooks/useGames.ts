bf1fb11f764b5ee05ce075781cd24aa6bad7d422
Preman <roshni.preman@bt.com>
2023-06-05 08:04:12 +0530

    Refactored games fetch fn with react query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,28 +1,36 @@
 import { GameQuery } from "../App";
-import useData from "./useData";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "../services/api-client";
+import { useQuery } from "@tanstack/react-query";
 
 export interface Platform {
-    id: number;
-    name: string;
-    slug: string;
+  id: number;
+  name: string;
+  slug: string;
 }
 
 export interface Game {
-    id: number;
-    background_image: string;
-    name: string;
-    parent_platforms: { platform: Platform}[];
-    metacritic: number;
-    rating_top: number;
+  id: number;
+  background_image: string;
+  name: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+  rating_top: number;
 }
 
-const useGames = (selectedGameQuery: GameQuery) => useData<Game>('/games', 
-        { params: { 
-            genres: selectedGameQuery.genre?.id, 
+const useGames = (selectedGameQuery: GameQuery) =>
+  useQuery<Game[], Error>({
+    queryKey: ["games", selectedGameQuery],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Game>>("games", {
+          params: {
+            genres: selectedGameQuery.genre?.id,
             platforms: selectedGameQuery.platform?.id,
             ordering: selectedGameQuery.order,
-            search: selectedGameQuery.search
-        } }, 
-        [selectedGameQuery]);
-
+            search: selectedGameQuery.search,
+          },
+        })
+        .then((data) => data.data.results),
+  });
 export default useGames;
